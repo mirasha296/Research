15e279df978cbaf153988cb1bae11fb7826de809
Lee Stemkoski <stemkoski@gmail.com>
2026-02-19 17:44:35 -0500

    first try at move controls

--- 3D_A_Frame/test-controls.js
+++ 3D_A_Frame/test-controls.js
@@ -2,13 +2,44 @@ AFRAME.registerComponent('test-controls',
 {
     init: function () 
     {
-        this.el.addEventListener('thumbstickmoved', function (evt)
+        /*
+            this runs 100 times per second on VR headsets
+        */
+        function move(event)
         {
-            let box = document.getElementById("box");
-            if (evt.detail.y > 0.95)  { box.setAttribute("color", "pink"); }
-            if (evt.detail.y < -0.95) { box.setAttribute("color", "red"); }
-            if (evt.detail.x < -0.95) { box.setAttribute("color", "green"); }
-            if (evt.detail.x > 0.95)  { box.setAttribute("color", "blue"); }
-        });
+
+            // how far should we move? (distance)
+            // speed = distance / time
+            // distance = speed * time
+
+            let x = event.detail.x / 100;
+            let z = event.detail.y / 100;
+
+            let camera = document.getElementById("camera");
+            
+            // get raw data
+            let positionString = camera.getAttribute("position"); // "4 3 7"
+
+            // convert to array (of strings)
+            let stringArray = positionString.split(" "); // ["4", "3", "7"]
+
+            // convert string to numbers
+            let numberArray = stringArray.map(Number); // [4, 3, 7]
+
+            // add or subtract from the number
+            numberArray[0] += x;
+            numberArray[2] += z;
+
+            // convert numbers back to strings
+            stringArray = numberArray.map(String);
+
+            // put strings back together separated by spaces
+            positionString = stringArray.join(" ");
+
+            // set attribute
+            camera.setAttribute("position", positionString);
+        }
+
+        this.el.addEventListener('thumbstickmoved',  move );
     }
 });
