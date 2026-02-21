2ca5f70db0dd4c8081558a23a95c09cd0f9f29ac
Myra <mirasha296@gmail.com>
2026-02-20 22:06:14 -0500

    testing to main cmykcube
    adding js function from test-cotrols to the cmyk cube

---
+++ 3D_A_Frame/CMYK_Cube.js
@@ -0,0 +1,28 @@
+AFRAME.registerComponent('test-controls',  
+{
+    init: function () 
+    {
+        /*
+            this runs 100 times per second on VR headsets
+        */
+        function move(event) {
+
+            let x = event.detail.x / 1000;
+            let z = event.detail.y / 1000;
+
+            let camera = document.getElementById("entity");
+
+            // get position as object
+            let position = camera.getAttribute("position");
+
+            // modify directly
+            position.x += x;
+            position.z += z;
+
+            // set it back
+            camera.setAttribute("position", position);
+        }
+
+        this.el.addEventListener('thumbstickmoved',  move );
+    }
+});
